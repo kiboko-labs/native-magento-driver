b4bcc18f6dae6f3e708af45f39207ffbcbdfc7ea
Grégory Planchat <gregory@luni.fr>
2015-12-29 02:59:36 +0100

    Added ProductRepositoryInterface

---
+++ src/MagentoDriver/Repository/ProductRepositoryInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Luni\Component\MagentoDriver\Repository;
+
+use Doctrine\Common\Collections\Collection;
+use Luni\Component\MagentoDriver\Entity\ProductInterface;
+use Luni\Component\MagentoDriver\Family\FamilyInterface;
+
+interface ProductRepositoryInterface
+{
+    /**
+     * @param string $code
+     * @return ProductInterface
+     */
+    public function findOneByIdentifier($code);
+
+    /**
+     * @param int $id
+     * @return ProductInterface
+     */
+    public function findOneById($id);
+
+    /**
+     * @param array $identifierList
+     * @return Collection|ProductInterface[]
+     */
+    public function findAllByIdentifier(array $identifierList);
+
+    /**
+     * @param array|int[] $idList
+     * @return Collection|ProductInterface[]
+     */
+    public function findAllById(array $idList);
+
+    /**
+     * @param FamilyInterface $family
+     * @return Collection|ProductInterface[]
+     */
+    public function findAllByFamily(FamilyInterface $family);
+
+    /**
+     * @return Collection|ProductInterface[]
+     */
+    public function findAll();
+}
