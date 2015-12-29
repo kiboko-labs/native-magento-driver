0b18629ca33699b50a11e36ee0a82f7557271d41
Grégory Planchat <gregory@luni.fr>
2015-12-29 02:04:03 +0100

    Added static attribute backend

--- src/MagentoDriver/Backend/Attribute/StaticAttributeBackend.php
+++ src/MagentoDriver/Backend/Attribute/StaticAttributeBackend.php
@@ -1,26 +1,22 @@
 <?php
 
-namespace Luni\Component\MagentoDriver\Backend\Attribute\StaticAttributes;
+namespace Luni\Component\MagentoDriver\Backend\Attribute;
 
-use Luni\Component\MagentoDriver\Backend\Attribute\BackendInterface;
 use Luni\Component\MagentoDriver\AttributeValue\AttributeValueInterface;
 use Luni\Component\MagentoDriver\Entity\ProductInterface;
 
-class SkuAttributeBackend
+class StaticAttributeBackend
     implements BackendInterface
 {
     public function persist(ProductInterface $product, AttributeValueInterface $value)
     {
-        // TODO: Implement persist() method.
     }
 
     public function initialize()
     {
-        // TODO: Implement initialize() method.
     }
 
     public function flush()
     {
-        // TODO: Implement flush() method.
     }
 }
