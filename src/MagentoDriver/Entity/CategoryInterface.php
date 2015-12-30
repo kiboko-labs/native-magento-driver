b121097f347b64d727b1ca4bccc7e67249c679fb
Grégory Planchat <gregory@luni.fr>
2015-12-30 23:12:49 +0100

    Added CategoryInterface

--- src/MagentoDriver/Entity/CategoryInterface.php
+++ src/MagentoDriver/Entity/CategoryInterface.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gplanchat
- * Date: 30/12/2015
- * Time: 16:55
- */
 
 namespace Luni\Component\MagentoDriver\Entity;
 
-
-class CategoryInterface
+interface CategoryInterface
+    extends EntityInterface
 {
-
 }
