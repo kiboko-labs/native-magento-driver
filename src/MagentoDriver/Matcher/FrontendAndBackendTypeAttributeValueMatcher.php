df448d13cdb6166b313924354e3451a8352dc068
Grégory Planchat <gregory@luni.fr>
2016-01-04 23:17:50 +0100

    Added new attribute value matcher

--- src/MagentoDriver/Matcher/FrontendAndBackendTypeAttributeValueMatcher.php
+++ src/MagentoDriver/Matcher/FrontendAndBackendTypeAttributeValueMatcher.php
@@ -4,7 +4,7 @@ namespace Luni\Component\MagentoDriver\Matcher;
 
 use Luni\Component\MagentoDriver\Model\AttributeInterface;
 
-class BackendTypeAttributeValueMatcher
+class FrontendAndBackendTypeAttributeValueMatcher
     implements AttributeValueMatcherInterface
 {
     /**
