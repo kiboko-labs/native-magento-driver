fe2d3589838110bc60fb544e350f0c11c69e1042
Grégory Planchat <gregory@luni.fr>
2016-03-09 00:08:49 +0100

    Added mappers

---
+++ src/MagentoMapper/Mapper/AttributeMapperInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Luni\Component\MagentoMapper\Mapper;
+
+use Luni\Component\MagentoDriver\Model\AttributeInterface;
+
+interface AttributeMapperInterface
+{
+    /**
+     * @param string $identifier
+     * @return AttributeInterface
+     */
+    public function map($identifier);
+}
