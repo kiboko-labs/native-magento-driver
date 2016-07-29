999d103f6e0424088134e70907eb99ac15db409f
Grégory Planchat <gregory@luni.fr>
2016-07-29 16:34:54 +0200

    Fixed dummy repository

--- src/MagentoDriver/Repository/Dummy/ProductUrlRewriteRepository.php
+++ src/MagentoDriver/Repository/Dummy/ProductUrlRewriteRepository.php
@@ -4,13 +4,30 @@ namespace Kiboko\Component\MagentoDriver\Repository\Dummy;
 
 use Kiboko\Component\MagentoDriver\Entity\Product\ProductInterface;
 use Kiboko\Component\MagentoDriver\Model\ProductInventoryInterface;
+use Kiboko\Component\MagentoDriver\Model\ProductUrlRewriteInterface;
 use Kiboko\Component\MagentoDriver\Repository\ProductInventoryRepositoryInterface;
 use Kiboko\Component\MagentoDriver\Repository\ProductPriceRepositoryInterface;
 use Kiboko\Component\MagentoDriver\Repository\ProductUrlRewriteRepositoryInterface;
 
 class ProductUrlRewriteRepository implements ProductUrlRewriteRepositoryInterface
 {
-    public function findOneByProduct(ProductInterface $product)
+    /**
+     * @param string $identifier
+     * @param int $storeId
+     *
+     * @return ProductUrlRewriteInterface
+     */
+    public function findOneByProductId($identifier, $storeId)
+    {
+        return null;
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @param int $storeId
+     * @return ProductUrlRewriteInterface
+     */
+    public function findOneByProduct(ProductInterface $product, $storeId)
     {
         return null;
     }
