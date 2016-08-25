2fa9ee28efc6636c4323d7472695a994a5cd7a99
Grégory Planchat <gregory@luni.fr>
2016-08-25 19:00:47 +0200

    Added cached store repository

---
+++ src/MagentoDriver/Repository/CachedRepository/CachedStoreRepository.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Kiboko\Component\MagentoDriver\Repository\CachedRepository;
+
+use Kiboko\Component\MagentoDriver\Model\StoreInterface;
+use Kiboko\Component\MagentoDriver\Repository\StoreRepositoryInterface;
+
+class CachedStoreRepository implements StoreRepositoryInterface
+{
+    /**
+     * @var StoreRepositoryInterface
+     */
+    private $decorated;
+
+    /**
+     * @var StoreInterface[]
+     */
+    private $cacheById;
+
+    /**
+     * @var StoreInterface[]
+     */
+    private $cacheByCode;
+
+    /**
+     * CachedStoreRepository constructor.
+     *
+     * @param StoreRepositoryInterface $storeRepository
+     */
+    public function __construct(StoreRepositoryInterface $storeRepository)
+    {
+        $this->decorated = $storeRepository;
+        $this->cacheById = [];
+        $this->cacheByCode = [];
+    }
+
+    /**
+     * @param int $identifier
+     *
+     * @return StoreInterface|null
+     */
+    public function findOneById($identifier)
+    {
+        if (isset($this->cacheById[$identifier])) {
+            return $this->cacheById[$identifier];
+        }
+
+        $store = $this->decorated->findOneById($identifier);
+        if ($store === null) {
+            return;
+        }
+
+        $this->cacheById[$store->getId()] = $store;
+        $this->cacheByCode[$store->getCode()] = $store;
+
+        return $store;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return StoreInterface|null
+     */
+    public function findOneByCode($code)
+    {
+        if (isset($this->cacheByCode[$code])) {
+            return $this->cacheByCode[$code];
+        }
+
+        $store = $this->decorated->findOneByCode($code);
+        if ($store === null) {
+            return;
+        }
+
+        $this->cacheById[$store->getId()] = $store;
+        $this->cacheByCode[$store->getCode()] = $store;
+
+        return $store;
+    }
+
+    /**
+     * @return StoreInterface[]|\Traversable
+     */
+    public function findAll()
+    {
+        return array_values($this->cacheById);
+    }
+}
